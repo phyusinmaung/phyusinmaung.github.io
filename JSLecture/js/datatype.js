ff414e4497e4a2f55112af4accda0598a5f924c6
phyusinmaung <phyusinmaung728@gmail.com>
2026-01-14 22:35:47 +0630

    JavaScript datatypes & variables

---
+++ JSLecture/js/datatype.js
@@ -0,0 +1,24 @@
+// String data type
+let name = "Phyu Sin Maung";
+console.log(name);
+console.log(typeof(name));
+
+// Numeric data type
+let age = 21;
+console.log(age);
+console.log(typeof(age));
+
+// Boolean data type 0,1
+let single = true;
+console.log(single);
+console.log(typeof(single));
+
+// Undefined
+let myBaby;
+console.log(myBaby);
+
+// Null
+let son = null;
+console.log(son);
+console.log(typeof(son));
+
